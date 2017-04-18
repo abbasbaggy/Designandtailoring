084a4153a35b6a2a4eda78066af4822bdb02ea50
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-18 23:50:04 +0100

    allow

--- admin/view.php
+++ admin/view.php
@@ -82,7 +82,7 @@ include('dashboard.php')
                     $sql2 = "select filename from tbl_files WHERE username = '$username'";
                     $result2 = mysqli_query($con, $sql2);
                     $row2 = mysqli_fetch_assoc($result2); ?>
-                    <a href="upload/<?php echo $row2['filename']; ?>" target="_blank">design veiw</a>
+                    <a href="../upload/<?php echo $row2['filename']; ?>" target="_blank">design veiw</a>
                 </td>
             </tr>
             <?php $count++; } ?>
