65c0590370884fd285bade9072688ed79d76e9bf
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-19 00:42:31 +0100

    allow

--- delete.php
+++ delete.php
@@ -5,11 +5,14 @@
  * Date: 3/31/2017
  * Time: 6:25 PM
  */
+include "auth.php";
+
+$username = $_SESSION["username"];
 require('db.php');
 $id=$_REQUEST['id'];
 $query = "DELETE FROM new_record WHERE id=$id";
 $result = mysqli_query($con,$query) or die ( mysqli_error());
-$query2= "DELETE FROM tbl_files WHERE id=$id";
+$query2= "DELETE FROM tbl_files WHERE username=$username";
 $result2= mysqli_query($con, $query2) or die (mysqli_error());
 header("Location: view.php");
 ?>
