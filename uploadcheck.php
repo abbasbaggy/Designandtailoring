c75ecf5d275f7bd831d475f2bc5be6be02cc8f5b
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-18 20:24:45 +0100

    allow

--- uploadcheck.php
+++ uploadcheck.php
@@ -5,7 +5,7 @@ include 'auth.php';
 $username = $_SESSION['username'];
 
 // fetch files
-$sql = "select filename from tbl_files WHERE username = '$username'";
+$sql = "select filename from tbl_files";
 $result = mysqli_query($con, $sql);
 
 if (!isset($_GET['delete_id'])){
