14dfbaca91f7c4f022d23ab375769df2507e7131
Abbas <abbas_yusuff2002@yahoo.co.uk>
2020-04-05 18:20:53 +0100

    new

--- inputs/insert flexible.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Abbas
- * Date: 4/2/2017
- * Time: 12:30 AM
- */
--- static/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Abbas
- * Date: 3/30/2017
- * Time: 7:06 PM
- */
